55efcd46811bef80d9ae9f13613cfdcf5ad1bca1
Otajon Makxmasoliyev <89655268+AbbosMaxmasoliyev@users.noreply.github.com>
2022-06-11 00:46:28 +0500

    Abbos home.js ni tugatdi

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import Logo  from "../images/logo/logo.png"
 import Navbar from '../Navbar';
 
+// images
+
+import Youtube from "../images/icons/youtube.png"
+import Instagram from "../images/icons/instagram.png"
+import Telegram from "../images/icons/telegram.png"
+import Facebook from "../images/icons/facebook.png"
+
 const Footer = () => {
   return (
-    <div>
+    <div className='footer'>
         <Navbar/>
         <hr/>
         <div className="block">
@@ -12,6 +19,19 @@ const Footer = () => {
             <p>Manzil: Samarqand shahar</p>
             <p>Universitet xiyoboni</p>
             <p>Mo'ljal: SamDU Tarix fakulteti hovlisi,</p>
+            </div>
+            <div className="center">
+                <div className="center_block">
+                    <a href="youtube.com"><img src={Youtube} alt="" /></a>
+                    <a href="instagram.com"><img src={Instagram} alt="" /></a>
+                    <a href="telegram.org"><img src={Telegram} alt="" /></a>
+                    <a href="facebook.com"><img src={Facebook} alt="" /></a>
+                </div>
+                <p style={{textTransform:"uppercase", fontSize:"12px"}}>yOSHLAR  TEXNOPARKI - Zamonaviy  ilm  poydevori!</p>
+            </div>
+            <div className="right">
+                <p>+998 88 934 55 05</p>
+                <p>samytpuz@gmail.com</p>
             </div>
         </div>
     </div>
